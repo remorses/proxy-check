c215b17c5963f2976dbacd10da8839f51789e9ed
Tommaso De Rossi <beats.by.morse@gmail.com>
2019-10-08 19:21:00 +0200

    Update __init__.py

--- proxy_check/__init__.py
+++ proxy_check/__init__.py
@@ -13,15 +13,15 @@ async def check(
     EXAMPLE_URL_HTTPS=EXAMPLE_URL_HTTPS,
 ):
     try:
-        client_session = aiohttp.ClientSession(raise_for_status=True)
-        if http:
-            resp = await client_session.get(EXAMPLE_URL_HTTP, proxy=url)
-            async with resp:
-                assert resp.status == 200
-        if https:
-            resp = await client_session.get(EXAMPLE_URL_HTTPS, proxy=url)
-            async with resp:
-                assert resp.status == 200
+        async with aiohttp.ClientSession(raise_for_status=True) as client_session:
+            if http:
+                resp = await client_session.get(EXAMPLE_URL_HTTP, proxy=url)
+                async with resp:
+                    assert resp.status == 200
+            if https:
+                resp = await client_session.get(EXAMPLE_URL_HTTPS, proxy=url)
+                async with resp:
+                    assert resp.status == 200
     except Exception as e:
         logger.warn(e)
         return False, e
